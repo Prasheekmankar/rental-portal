26e56ead6e57ed5d87165f5ac070dcbe5a6e2a15
Prasheekmankar <89675381+Prasheekmankar@users.noreply.github.com>
2022-04-03 22:40:01 +0530

    commit change

---
+++ src/bike.js
@@ -0,0 +1,127 @@
+const bike = [
+    {
+        id:"1",
+        poster:"https://www.drivespark.com/img/2021/04/royal-enfield-classic-350-single-channel-1618221940.jpg",
+        name:"Royal Enfild",
+        type:"Gear Bike",
+        mileage:"35",
+        seater:"Two-Seater",
+        daycost:"549",
+        weekcost:"3449",
+        quantity:"10",
+        category:"Classical Bike"
+    },
+     {
+        id:"2",
+        poster:"https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/classic-350/landing/classic-350-motorcycle.jpg",
+        name:"Royal Enfild",
+        type:"Gear Bike",
+        mileage:"35",
+        seater:"One-Seater",
+        daycost:"549",
+        weekcost:"3449",
+        quantity:"10",
+        category:"Classical Bike"
+    },
+    {
+        id:"3",
+        poster:"https://c.ndtvimg.com/2019-06/mrv2a398_ktm-125-duke_120x90_06_June_19.jpg",
+        name:"KTM Duke",
+        type:"Gear Bike",
+        mileage:"35",
+        seater:"Two-Seater",
+        daycost:"449",
+        weekcost:"2999",
+        quantity:"10",
+        category:"Sports Bike",
+    },
+    {
+        id:"4",
+        poster:"https://cdn.autoportal.com/bp-v3/img/models/9d/a/2014-hereo-karizma-zmr-motorcycle-pic-image-photo-review-road-test-24082014-m12_560x420.jpg",
+        name:"karizma",
+        type:"Gear Bike",
+        mileage:"50",
+        seater:"Two-Seater",
+        daycost:"449",
+        weekcost:"2999",
+        quantity:"10",
+        category:"Sports Bike",
+    },
+    {
+        id:"5",
+        poster:"https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2018/04/06/Pictures/_47cb4038-397e-11e8-90dd-823da00706aa.jpg",
+        name:"R15",
+        type:"Gear Bike",
+        mileage:"50",
+        seater:"Two-Seater",
+        daycost:"449",
+        weekcost:"2999",
+        quantity:"10",
+        category:"Sports Bike"
+    },
+    {
+        id:"6",
+        poster:"https://c.ndtvimg.com/2020-02/rek00l_2020-hero-passion-pro_625x300_24_February_20.jpg",
+        name:"Passion Pro",
+        type:"Gear Bike",
+        mileage:"35",
+        seater:"Two-Seater",
+        daycost:"399",
+        weekcost:"2699",
+        quantity:"10",
+        category:"Regular Bike"
+    },
+    {
+        id:"7",
+        poster:"https://media.zigcdn.com/media/photogallery/2016/Jan/tvs-victor-110-review-zigwheels-india-g07_320x240.jpg",
+        name:"Bajaj Discover",
+        type:"Gear Bike",
+        mileage:"70",
+        seater:"Two-Seater",
+        daycost:"399",
+        weekcost:"2699",
+        quantity:"10",
+        category:"Regular Bike"
+    },
+    {
+        id:"8",
+        poster:"https://ic1.maxabout.us/autos/tw_india//H/2021/6/hero-splendor-plus-black-and-accent-edition.jpg",
+        name:"Splendor-plus",
+        type:"Gear Bike",
+        mileage:"70",
+        seater:"Two-Seater",
+        daycost:"399",
+        weekcost:"2699",
+        quantity:"10",
+        category:"Regular Bike"
+    },
+    {
+        id:"9",
+        poster:"https://media.zigcdn.com/media/model/2020/Feb/right-side-view-2025944673_600x400.jpg",
+        name:"Honda Activa",
+        type:"Moped",
+        mileage:"50",
+        seater:"Two-Seater",
+        daycost:"399",
+        weekcost:"2699",
+        quantity:"10",
+        category:"Moped"
+    },
+    {
+        id:"10",
+        poster:"https://www.drivespark.com/img/2019/05/hero-pleasure-plus-110-side-profile-1557928538.jpg",
+        name:"Hero-pleasure-plus",
+        type:"Moped",
+        mileage:"60",
+        seater:"Two-Seater",
+        daycost:"399",
+        weekcost:"2699",
+        quantity:"10",
+        category:"Moped"
+    },
+];
+
+
+
+
+console.log(bike);
